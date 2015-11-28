45d4c1ebe5a35d46a18a0e5a3bc34da3215db177
Wilfred Chong <wilfredcy@gmail.com>
2015-11-28 16:33:21 +0800

    image upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,10 @@
 class Pin < ActiveRecord::Base
+	
+	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.2-Q16'
 	belongs_to :user
+
+	#has_attached_file :image, styles: { medium: "1000x1000>"}
+	has_attached_file :image, styles: { medium: "300x300>"}
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	#validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 end
