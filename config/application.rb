a896956a5b98e58f17563d604a4cf7cf8cc4838a
WilfredChong <wilfredcy@gmail.com>
2015-11-29 21:53:05 +0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module PinBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
     #Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
   end
 end
